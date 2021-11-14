66240aceda0b5ac8f5418db0d07bcec544b6b0b2
kamilkulikTeck <kulikkamil@gmail.com>
2021-11-15 00:01:26 +0100

    PoW python blockchain - update Block class with mine method

--- PoW-blockchain-python/block.py
+++ PoW-blockchain-python/block.py
@@ -4,11 +4,18 @@ from hashlib import sha256
 
 class Block:
     def __init__(self, data=[], timestamp="") -> None:
-        self.prev_hash = ""
         self.data = data
+        self.nonce = 0
+        self.prev_hash = ""
         self.timestamp = timestamp
         self.hash = self.getHash()
-        # self.nonce = nonce
 
     def getHash(self):
-        return sha256((self.prev_hash + self.timestamp + json.dumps(self.data)).encode("utf-8")).hexdigest()
+        return sha256(
+            (str(self.nonce) + self.prev_hash + self.timestamp + json.dumps(self.data)).encode("utf-8")
+        ).hexdigest()
+
+    def mine(self, difficulty):
+        while not self.hash.startswith((difficulty + 1) * "0"):
+            self.nonce += 1
+            self.hash = self.getHash()
